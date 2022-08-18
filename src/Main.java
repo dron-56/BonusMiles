beaf7b676462c3af32739d696f5ffe89304ebc70
dron56 <dron23mk@gmail.com>
2022-08-18 15:11:09 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 2500; // стоимость билета
+        int mile = 20; // цена одной мили
+        int miles = ticket / mile; // количество бонусных миль
+
+        System.out.println(miles + " бонусных(ая) миль(я)");
+    }
+}
+
